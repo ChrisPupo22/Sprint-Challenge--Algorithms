7fa103777444d03c8ced89dfab20b30796efeb61
Christopher Pupo <ChrisPupo@Christophers-MacBook-Pro.local>
2020-02-21 16:01:29 -0500

    close but oh so far fromt the robot sort!!

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,26 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        self.set_light_on()
+        self.swap_item()
+
+        while self.light_is_on(): 
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                if self.can_move_right() == False and self.compare_item() == None: 
+                    self.swap_item() 
+                    break ##??? not sure if this is the correct way to breakout here...
+
+                if self.can_move_left():
+                    self.move_left()
+                    if self.compare_item() == None:
+                        self.swap_item()
+                        self.move_right()
+                        self.swap_item()
+                        break ##?? same issue 
+                    
 
 
 if __name__ == "__main__":
